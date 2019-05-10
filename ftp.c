3684192da26b3a92743274538d87924301759f6d
irioborges <iriormb@gmail.com>
2019-05-10 11:36:03 -0300

    Commit com atualização dos comando: user, pass e quit

--- ftp.c
+++ ftp.c
@@ -1,4 +1,3 @@
-// Server side C/C++ program to demonstrate Socket programming 
 #include <unistd.h> 
 #include <stdio.h> 
 #include <sys/socket.h> 
@@ -7,76 +6,174 @@
 #include <string.h> 
 #define PORT 21 
 
-int main(int argc, char const *argv[]) 
-{ 
-	int server_fd, new_socket, valread; 
-	struct sockaddr_in address; 
-	int opt = 1; 
-	int addrlen = sizeof(address); 
-	char buffer[1024] = {0}; 
-	char *hello = "220 Hello from server \n"; 
+typedef struct State {
+  /* Connection mode: NORMAL, SERVER, CLIENT */
+  int mode;
+
+  /* Is user loggd in? */
+  int logged_in;
+
+  /* Is this username allowed? */
+  int username_ok;
+  char *username;
+  
+  /* Response message to client e.g. 220 Welcome */
+  char *message;
+
+  /* Commander connection */
+  int connection;
+
+  /* Socket for passive connection (must be accepted later) */
+  int sock_pasv;
+
+  /* Transfer process id */
+  int tr_pid;
+
+} State;
+
+
+typedef struct Command {
+  char command[5];
+  char arg[1024];
+} Command;
+
+
+void parse_command(char *cmdstring, Command *cmd)
+{
+  sscanf(cmdstring,"%s %s",cmd->command,cmd->arg);
+}
+
+void response(Command *cmd, State *state) {
+/*  switch(lookup_cmd(cmd->command)){
+    case USER: ftp_user(cmd,state); break;
+    case PASS: ftp_pass(cmd,state); break;
+    case PASV: ftp_pasv(cmd,state); break;
+    case LIST: ftp_list(cmd,state); break;
+    case CWD:  ftp_cwd(cmd,state); break;
+    case PWD:  ftp_pwd(cmd,state); break;
+    case MKD:  ftp_mkd(cmd,state); break;
+    case RMD:  ftp_rmd(cmd,state); break;
+    case RETR: ftp_retr(cmd,state); break;
+    case STOR: ftp_stor(cmd,state); break;
+    case DELE: ftp_dele(cmd,state); break;
+    case SIZE: ftp_size(cmd,state); break;
+    case ABOR: ftp_abor(state); break;
+    case QUIT: ftp_quit(state); break;
+    case TYPE: ftp_type(cmd,state); break;
+    case NOOP:
+      if(state->logged_in){
+        state->message = "200 Nice to NOOP you!\n";
+      }else{
+        state->message = "530 NOOB hehe.\n";
+      }
+      write_state(state);
+      break;
+    default: 
+      state->message = "500 Unknown command\n";
+      write_state(state);
+      break;
+  } */
+
+
+if(strcmp(cmd->command, "USER") == 0){
+  printf("\n%s\n", cmd->command);
+  state->message = "331 Usuario Ok\n";
+}
+
+if(strcmp(cmd->command, "PASS") == 0){
+  printf("\n%s\n", cmd->command);
+  state->message = "230 Login realizado com sucesso!\n";
+}
+
+if(strcmp(cmd->command, "SYST") == 0){
+  printf("\n%s\n", cmd->command);
+  state->message = "Linux\n";
+}
+
+if(strcmp(cmd->command, "QUIT") == 0){
+  printf("\n%s\n", cmd->command);
+  state->message = "221 Tchau!!\n";
+  close(state->connection);
+  exit(0);
+}
+
+
+write(state->connection, state->message, strlen(state->message));
+
+}
+
+int main(int argc, char const *argv[]) { 
+  int server_fd, new_socket, valread; 
+  struct sockaddr_in address; 
+  int addrlen = sizeof(address); 
+  char buffer[1024] = {0}; 
+  Command *cmd = malloc(sizeof(Command));
+  State *state = malloc(sizeof(State));
+  char *hello = "220 Seja bem vindo\n"; 
 	
-	// Creating socket file descriptor 
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-	{ 
-		perror("socket failed"); 
-		exit(EXIT_FAILURE); 
-	} 
+  //Cria o socket
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
+    perror("socket failed"); 
+    exit(EXIT_FAILURE); 
+  } 
 	
-	address.sin_family = AF_INET; 
-	address.sin_addr.s_addr = INADDR_ANY; 
-	address.sin_port = htons( PORT ); 
+  address.sin_family = AF_INET; 
+  address.sin_addr.s_addr = INADDR_ANY; 
+  address.sin_port = htons( PORT ); 
 	
-	// Forcefully attaching socket to the port
-	if (bind(server_fd, (struct sockaddr *)&address, 
-								sizeof(address))<0) 
-	{ 
-		perror("bind failed"); 
-		exit(EXIT_FAILURE); 
-	} 
-	if (listen(server_fd, 3) < 0) 
-	{ 
-		perror("listen"); 
-		exit(EXIT_FAILURE); 
-	} 
+  //Associa o socket a porta
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) { 
+    perror("bind failed"); 
+    exit(EXIT_FAILURE); 
+  } 
+
+  //Escuta o socket na porta
+  if (listen(server_fd, 3) < 0) { 
+    perror("listen"); 
+    exit(EXIT_FAILURE); 
+  } 
 	
-        while(1) {
-          if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) { 
-	    perror("accept"); 
-	    exit(EXIT_FAILURE); 
-	  } 
-  	  close(server_fd);
-	  write(new_socket, hello, strlen(hello)); 
-          while (valread = read(new_socket, buffer, 1024)){
-            printf("%s", buffer);
-            //signal(SIGCHLD,my_wait);
-            //  if(!(bytes_read>BSIZE)){
-            //    /* TODO: output this to log */
-            //    buffer[BSIZE-1] = '\0';
-            //    printf("User %s sent command: %s\n",(state->username==0)?"unknown":state->username,buffer);
-            //    parse_command(buffer,cmd);
-            //    state->connection = connection;
+  while(1) {
+    //Espera por requisição de conexão entrante		
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) { 
+      perror("accept"); 
+      exit(EXIT_FAILURE); 
+    }
+ 
+    printf("Realizada requisição de conexão entrante\n");
+    close(server_fd);
+
+    //Faz envio de mensagem de boas vindas ao cliente que fez a requisição de conexão entrante	
+    write(new_socket, hello, strlen(hello)); 
+
+    //Aguarda por comando do cliente
+    while (valread = read(new_socket, buffer, 1024)){
+      printf("%s", buffer);
+      //signal(SIGCHLD,my_wait);
+      if(!(valread > 1024)){
+        buffer[1023] = '\0';
+        printf("User %s sent command: %s\n",(state->username==0)?"unknown":state->username,buffer);
+        parse_command(buffer,cmd);
+        state->connection = new_socket;
           
-            /* Ignore non-ascii char. Ignores telnet command */
-            //    if(buffer[0]<=127 || buffer[0]>=0){
-            //      response(cmd,state);
-            //    }
-            //    memset(buffer,0,BSIZE);
-            //    memset(cmd,0,sizeof(cmd));
-            //  }else{
-            /* Read error */
-            //   perror("server:read");
-            // }
-           }
-  	    
-	    valread = read(new_socket, buffer, 1024);
-     	    
-            hello = "";
-            write(new_socket, hello, strlen(hello)); 
-   	    valread = read(new_socket, buffer, 1024);
-    	    printf("%s", buffer);
-          //}
+        /* Ignore non-ascii char. Ignores telnet command */
+        if(buffer[0]<=127 || buffer[0]>=0){
+          response(cmd,state);
         }
-	return 0; 
-} 
+        //    memset(buffer,0,BSIZE);
+        //    memset(cmd,0,sizeof(cmd));
+        //  }else{
+        /* Read error */
+        //   perror("server:read");
+              // }
+      }
+    }//Fecha colchete do segundo loop
+    printf("Client disconnected.\n");
+    exit(0);	
+
+    close(new_socket);     
+          
+  }//Fecha colchete do loop principal
+
+}//Fecha colchete do main 
 
---
+++ readme.md
@@ -0,0 +1,8 @@
+#Servidor FTP
+## Aluno: Írio Rafael de Menezes Borges
+
+
+
+
+
+###"Este projeto foi desenvolvido integralmente pela equipe, sem ajuda não autorizada de alunos não membros do projeto no processo de codificação."
